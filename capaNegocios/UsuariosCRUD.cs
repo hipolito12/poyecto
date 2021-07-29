c8d92236ce01c74e433b585160527285f23d5461
hipolito la barba <labarbahipolito@hotmail.com>
2021-07-29 12:04:35 -0300

    Hacer form de usuarios

---
+++ capaNegocios/UsuariosCRUD.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using datos;
+using System.Data.SqlClient;
+using System.Data;
+
+
+
+namespace capaNegocios
+{
+    class UsuariosCRUD
+    {
+
+        private Usuarios U =  new Usuarios();
+
+
+        public DataTable muestra()
+        {
+            DataTable tabla = new DataTable();
+            tabla = U.listadoUsuario();
+            return tabla;
+
+        }
+
+        public void addp(string idper, string nombre, string nombreU, string apellido, string clave, string cambia, int habilit, string email)
+        {
+
+
+            U.insertUsuario(idper, nombre, nombreU,apellido,clave,cambia,Convert.ToInt32(habilit), email );
+
+
+
+        }
+
+        public void Editapstring (string idper, string nombre, string nombreU, string apellido, string clave, string cambia, int habilit, string email, string id)
+        {
+            U.updateUsuario(idper, nombre, nombreU, apellido, clave, cambia, Convert.ToInt32(habilit), email, Convert.ToInt32(id));
+        }
+
+        public void Eliminap(string id)
+        {
+            U.EliminarUsuario(Convert.ToInt32(id));
+        }
+
+    }
+}
+
---
+++ proecto prueba/Usuarios.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace datos
+{
+   public  class Usuarios
+       
+    {
+        private CD_Conexion conexion = new CD_Conexion();
+        SqlDataReader leer;
+        DataTable tabla = new DataTable();
+        SqlCommand comando = new SqlCommand();
+
+        
+
+
+
+
+        public DataTable listadoUsuario()
+        {
+            comando.Connection = conexion.AbrirConexion();
+            comando.CommandText = "MostrarMaterias";
+            comando.CommandType = CommandType.StoredProcedure;
+            leer = comando.ExecuteReader();
+            tabla.Load(leer);
+            conexion.CerrarConexion();
+            return tabla;
+        }
+
+
+        public void insertUsuario(string idper, string nombre,string nombreU,string apellido ,string clave, string cambia, int habilit,string email)
+        {
+            comando.Connection = conexion.AbrirConexion();
+            comando.CommandText = "insertarUsuario";
+            comando.CommandType = CommandType.StoredProcedure;
+            comando.Parameters.AddWithValue("@idpersona", idper);
+            comando.Parameters.AddWithValue("@nombreU", nombreU);
+            comando.Parameters.AddWithValue("@apellido", apellido);
+            comando.Parameters.AddWithValue("@nombre", nombre);
+            comando.Parameters.AddWithValue("@calve", clave);
+            comando.Parameters.AddWithValue("@cambia", cambia);
+            comando.Parameters.AddWithValue("@habilitado", habilit);
+            comando.Parameters.AddWithValue("@email", email);
+            comando.ExecuteNonQuery();
+            comando.Parameters.Clear();
+            conexion.CerrarConexion();
+        }
+
+
+        public void updateUsuario(string idper, string nombre, string nombreU, string apellido, string clave, string cambia, int habilit, string email,int id)
+        {
+            comando.Connection = conexion.AbrirConexion();
+            comando.CommandText = "updateUsuario";
+            comando.CommandType = CommandType.StoredProcedure;
+            comando.Parameters.AddWithValue("@idpersona", idper);
+            comando.Parameters.AddWithValue("@nombreU", nombreU);
+            comando.Parameters.AddWithValue("@apellido", apellido);
+            comando.Parameters.AddWithValue("@nombre", nombre);
+            comando.Parameters.AddWithValue("@calve", clave);
+            comando.Parameters.AddWithValue("@cambia", cambia);
+            comando.Parameters.AddWithValue("@habilitado", habilit);
+            comando.Parameters.AddWithValue("@email", email); ;
+            comando.Parameters.AddWithValue("@id", id);
+
+            comando.ExecuteNonQuery();
+            comando.Parameters.Clear();
+            conexion.CerrarConexion();
+        }
+
+
+        public void EliminarUsuario(int id)
+        {
+            comando.Connection = conexion.AbrirConexion();
+            comando.CommandText = "eliminaUsuarios";
+            comando.CommandType = CommandType.StoredProcedure;
+            comando.Parameters.AddWithValue("@id", id);
+            comando.ExecuteNonQuery();
+            comando.Parameters.Clear();
+            conexion.CerrarConexion();
+        }
+
+
+        public void consultaUsuario(int id)
+        {
+            comando.Connection = conexion.AbrirConexion();
+            comando.CommandText = "consultarUsuario";
+            comando.CommandType = CommandType.StoredProcedure;
+            comando.Parameters.AddWithValue("@id", id);
+
+            comando.ExecuteNonQuery();
+            comando.Parameters.Clear();
+            conexion.CerrarConexion();
+        }
+
+    }
+}
