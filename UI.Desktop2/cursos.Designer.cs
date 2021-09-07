17d1c8642274540ca60479421624f15cfa311b75
hipolito la barba <labarbahipolito@hotmail.com>
2021-09-07 18:07:58 -0300

    nuevos

---
+++ UI.Desktop2/cursos.Designer.cs
@@ -0,0 +1,47 @@
+﻿
+namespace UI.Desktop2
+{
+    partial class cursos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // cursos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(797, 450);
+            this.Name = "cursos";
+            this.Text = "cursos";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
