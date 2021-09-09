0416ca49f27e44172907de7546497560e7c42164
hipolito la barba <labarbahipolito@hotmail.com>
2021-09-09 13:19:39 -0300

    ejemplo log in

--- UI.Desktop2/Login.cs
+++ UI.Desktop2/Login.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using UI.Desktop2.database;
 namespace UI.Desktop2
 {
     public partial class Login : Form
@@ -25,7 +25,11 @@ namespace UI.Desktop2
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
             try {
+                personas p = new personas();
+                Entidad en = new Entidad();
+                var s = textBox1.Text;
                if( textBox1.Text.Length == 0 || textBox1.Text.Length == null) ;
+                personas per = en.personas.Where(pers=> pers.nombre == s).First();
             }
             catch(Exception en)
            
