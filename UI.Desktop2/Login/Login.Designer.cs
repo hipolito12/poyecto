7330b4a4c234eb5b26e441bfd389d87e6714c315
hipolito la barba <labarbahipolito@hotmail.com>
2021-09-30 11:17:26 -0300

    ultima

---
+++ UI.Desktop2/Login/Login.Designer.cs
@@ -0,0 +1,138 @@
+﻿
+namespace UI.Desktop2.Login
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtContrasena = new System.Windows.Forms.TextBox();
+            this.txtusuarios = new System.Windows.Forms.MaskedTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.btnaceptar = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 158);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Usuario";
+            // 
+            // txtContrasena
+            // 
+            this.txtContrasena.Location = new System.Drawing.Point(70, 213);
+            this.txtContrasena.Name = "txtContrasena";
+            this.txtContrasena.Size = new System.Drawing.Size(259, 20);
+            this.txtContrasena.TabIndex = 1;
+            this.txtContrasena.TextChanged += new System.EventHandler(this.txtContrasena_TextChanged);
+            // 
+            // txtusuarios
+            // 
+            this.txtusuarios.Location = new System.Drawing.Point(70, 158);
+            this.txtusuarios.Name = "txtusuarios";
+            this.txtusuarios.Size = new System.Drawing.Size(259, 20);
+            this.txtusuarios.TabIndex = 2;
+            this.txtusuarios.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.txtusuarios_MaskInputRejected);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 220);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Contraseña";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel1.Location = new System.Drawing.Point(12, 294);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(178, 17);
+            this.linkLabel1.TabIndex = 4;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "¿Olvidaste tu contraseña? ";
+            // 
+            // btnaceptar
+            // 
+            this.btnaceptar.Location = new System.Drawing.Point(145, 254);
+            this.btnaceptar.Name = "btnaceptar";
+            this.btnaceptar.Size = new System.Drawing.Size(83, 23);
+            this.btnaceptar.TabIndex = 5;
+            this.btnaceptar.Text = "Aceptar";
+            this.btnaceptar.UseVisualStyleBackColor = true;
+            this.btnaceptar.Click += new System.EventHandler(this.btnaceptar_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.richTextBox1.Font = new System.Drawing.Font("Leelawadee UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(15, 33);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(3, 3, 3, 2);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(333, 85);
+            this.richTextBox1.TabIndex = 6;
+            this.richTextBox1.Text = "                                    Bienvenido al sistemas !\n                    " +
+    "   Digite sus datos para poder ingresar!\n";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(381, 336);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btnaceptar);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtusuarios);
+            this.Controls.Add(this.txtContrasena);
+            this.Controls.Add(this.label1);
+            this.Name = "Login";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        public System.Windows.Forms.TextBox txtContrasena;
+        public System.Windows.Forms.MaskedTextBox txtusuarios;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        public System.Windows.Forms.Button btnaceptar;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
---
+++ UI.Desktop2/Login/Login.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UI.Desktop2.Login
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtusuarios_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+            if (txtusuarios.Text.Length > 8 && txtusuarios.Text != null)
+            {
+
+            }
+            else { MessageBox.Show("error en el usuario"); }
+
+           
+        }
+
+        private void txtContrasena_TextChanged(object sender, EventArgs e)
+        {
+            if (txtContrasena.Text.Length > 8 && txtContrasena.Text!= null)
+            {
+                
+            }
+            else { MessageBox.Show("error en la  contrasena"); }
+        }
+
+        private void btnaceptar_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
